d671b7bd50d03e35f9802c92b01fda241be40d01
BhanuGaur007 <bhanugaur007@gmail.com>
2023-01-11 20:31:59 +0530

    array

---
+++ hello.js
@@ -0,0 +1,4 @@
+console.log("hello")
+ a =10;
+ b =20;
+ console.log(a+b)
---
+++ test.js
@@ -0,0 +1,17 @@
+// Comparison operators 
+let d = 10; 
+let e = 10; 
+let f = "10"; 
+console.log("d == e   is " + d == e); 
+console.log("d === e   is " + d === e); 
+console.log("d == f   is " + d == f); 
+console.log("d != e   is " + d != e); 
+console.log("d === f   is " + d === f); 
+console.log("d !== f   is " + d !== f);
+
+
+
+let x=x+2
+console.log(x)
+// var x+=2
+// console.log(x)
